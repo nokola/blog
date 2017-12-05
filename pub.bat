45b9b7b7a12dac49ebaf106999257f9a7ad10644
Nokola <nikolami@outlook.com>
2017-12-05 14:48:03 -0800

    Bat file fix

--- pub.bat
+++ pub.bat
@@ -1,15 +1,17 @@
 @echo off
-echo Pushing orig...
-git add .
-git commit -m %1
-git push
-echo.
 echo Rebuild
 hugo
 echo.
+
 echo Pushing generated...
 cd public
 git add .
 git commit -m %1
 git push
 cd ..
+
+echo Pushing orig...
+git add .
+git commit -m %1
+git push
+echo.
